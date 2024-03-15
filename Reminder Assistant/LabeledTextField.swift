b7d6b70b1bbed05369798559b74b897ed189d10f
Masaki-JP <masakidayo0508@gmail.com>
2024-03-15 13:11:47 +0900

    feat: RepresentedUITextFieldを用いたLabeledTextFieldの作成

---
+++ Reminder Assistant/LabeledTextField.swift
@@ -0,0 +1,132 @@
+import SwiftUI
+
+struct LabeledTextField: View {
+    let title: String
+    let text: Binding<String>
+    let focusState: FocusState<ContentView.Focus?>.Binding
+    let focusCase: ContentView.Focus
+    let returnKeyType: UIReturnKeyType
+    let dismissKeyboardAfterCompletion: Bool
+    let onReturnAction: @MainActor () -> Void
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(title)
+            RepresentedUITextFieldWrapper(
+                text: text,
+                focusState: focusState,
+                focusCase: focusCase,
+                returnKeyType: returnKeyType,
+                dismissKeyboardAfterCompletion: dismissKeyboardAfterCompletion,
+                onReturnAction: onReturnAction
+            )
+            Rectangle()
+                .frame(maxWidth: .infinity)
+                .frame(height: 1)
+        }
+        .onTapGesture {
+            focusState.wrappedValue = focusCase
+        }
+    }
+}
+
+private struct RepresentedUITextFieldWrapper: View {
+    let text: Binding<String>
+    let focusState: FocusState<ContentView.Focus?>.Binding
+    let focusCase: ContentView.Focus
+    let returnKeyType: UIReturnKeyType
+    let dismissKeyboardAfterCompletion: Bool
+    let onReturnAction: @MainActor () -> Void
+
+    var body: some View {
+        TextField("", text: text)
+            .disabled(true)
+            .foregroundStyle(.clear)
+            .overlay {
+                RepresentedUITextField(
+                    text: text,
+                    returnKeyType: returnKeyType,
+                    dismissKeyboardAfterCompletion: dismissKeyboardAfterCompletion,
+                    onReturnAction: onReturnAction
+                )
+                .focused(focusState, equals: focusCase)
+            }
+    }
+}
+
+private struct RepresentedUITextField: UIViewRepresentable {
+    let text: Binding<String>
+    let returnKeyType: UIReturnKeyType
+    let dismissKeyboardAfterCompletion: Bool
+    let onReturnAction: @MainActor () -> Void
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField(frame: .zero)
+        textField.returnKeyType = returnKeyType
+        textField.enablesReturnKeyAutomatically = true
+        textField.delegate = context.coordinator
+        textField.font = UIFont.preferredFont(forTextStyle: .body)
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text.wrappedValue
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(
+            text: text,
+            dismissKeyboardAfterCompletion: dismissKeyboardAfterCompletion,
+            onReturnAction: onReturnAction
+        )
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var text: Binding<String>
+        let dismissKeyboardAfterCompletion: Bool
+        let onReturnAction: @MainActor () -> Void
+
+        init(
+            text: Binding<String>,
+            dismissKeyboardAfterCompletion: Bool,
+            onReturnAction: @escaping () -> Void
+        ) {
+            self.text = text
+            self.dismissKeyboardAfterCompletion = dismissKeyboardAfterCompletion
+            self.onReturnAction = onReturnAction
+        }
+
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            self.text.wrappedValue = textField.text ?? ""
+            onReturnAction()
+            if dismissKeyboardAfterCompletion == true {
+                textField.resignFirstResponder()
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
+
+private let labeledTextFieldSample = LabeledTextField(
+    title: "期限",
+    text: Binding.constant("明日の夜"),
+    focusState: FocusState<ContentView.Focus?>().projectedValue,
+    focusCase: .title,
+    returnKeyType: .default,
+    dismissKeyboardAfterCompletion: false,
+    onReturnAction: {}
+)
+
+#Preview("Light") {
+    labeledTextFieldSample
+        .preferredColorScheme(.light)
+        .padding(.horizontal)
+}
+
+#Preview("Dark") {
+    labeledTextFieldSample
+        .preferredColorScheme(.dark)
+        .padding(.horizontal)
+}
