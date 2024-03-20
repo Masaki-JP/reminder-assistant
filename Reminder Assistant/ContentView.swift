414833fbe4af65627e70b5ced3f7e0aeddcdf2dd
Masaki-JP <masakidayo0508@gmail.com>
2024-03-21 06:24:18 +0900

    change: ContentViewでモーダル表示を行うSettingsViewのpresentationDetentsを設定

--- Reminder Assistant/ContentView.swift
+++ Reminder Assistant/ContentView.swift
@@ -66,6 +66,7 @@ struct ContentView: View {
         }
         .sheet(isPresented: $isShowSettingView) {
             SettingsView()
+                .presentationDetents([.medium])
         }
     }
 
